eb6a286a617dc568b4fe08c38bdce62ae4dd0b31
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-07 13:00:31 +0530

    add constant file

---
+++ app/src/main/java/com/example/tictactoeassignment/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.tictactoeassignment
+
+object Constant {
+    const val TAG = "JAPAN"
+}
--- app/src/main/java/com/example/tictactoeassignment/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeassignment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoeassignment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,21 +11,46 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tictactoeassignment.Constant.TAG
 import com.example.tictactoeassignment.screens.GameBoard
 import com.example.tictactoeassignment.ui.theme.TicTacToeAssignmentTheme
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TicTacToeAssignmentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GameBoard()
-                }
+
+        FirebaseApp.initializeApp(this)
+        val db = Firebase.firestore
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w(TAG, "Error adding document", e)
+//            }
+
+        val docRef = db.collection("users").document("xGNJ0lTpS45f8XKx0YE0")
+        docRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d(TAG, "Current data: ${snapshot.data}")
+            } else {
+                Log.d(TAG, "Current data: null")
             }
         }
     }
