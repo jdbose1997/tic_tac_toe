a4e5d1aac7f519bace4e14b165dc58e053490663
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-10 11:15:22 +0530

    create game room UI

--- app/src/main/java/com/example/tictactoeassignment/screens/GameRoomScreen.kt
+++ app/src/main/java/com/example/tictactoeassignment/screens/GameRoomScreen.kt
@@ -1,6 +1,9 @@
 package com.example.tictactoeassignment.screens
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -8,6 +11,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.dp
+import com.example.data.model.GameRoom
 import com.example.data.model.Player
 import com.example.tictactoeassignment.viewModels.GameRoomViewModel
 import com.example.tictactoeassignment.viewModels.LoginViewModel
@@ -16,10 +22,24 @@ import java.util.*
 @Composable
 fun GameRoomScreen(viewModel: GameRoomViewModel){
 
-    val listOfPlayer = viewModel.roomList.collectAsState().value
-    LazyColumn(modifier = Modifier.fillMaxHeight()) {
-        items(listOfPlayer){player->
-            Text(text = player.currentPlayers.toString())
+    val listOfGameRoom = viewModel.roomList.collectAsState().value
+    LazyColumn(modifier = Modifier.fillMaxHeight().padding(12.dp)) {
+        items(listOfGameRoom){gameRoom->
+           RoomCard(gameRoom = gameRoom)
         }
     }
+}
+
+@Composable
+fun RoomCard(gameRoom : GameRoom){
+    Text(text = "Room Name : ${gameRoom.roomName}" )
+    Spacer(modifier = Modifier.height(10.dp))
+    Text(text = "Players : ${gameRoom.currentPlayers}/2" )
+    Spacer(modifier = Modifier.height(10.dp))
+    Button(onClick = {
+
+    }, modifier = Modifier.alpha(if(gameRoom.currentPlayers < 2) 1f else 0.5f), enabled = gameRoom.currentPlayers < 2) {
+        Text("Join")
+    }
+    Spacer(modifier = Modifier.height(10.dp))
 }
