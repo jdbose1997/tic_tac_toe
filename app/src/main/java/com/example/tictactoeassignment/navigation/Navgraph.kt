9fcb41f58170baeb5f6ddd1c4241a6e241df2391
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-10 12:42:59 +0530

    add navigation realted code

---
+++ app/src/main/java/com/example/tictactoeassignment/navigation/Navgraph.kt
@@ -0,0 +1,28 @@
+package com.example.tictactoeassignment.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.tictactoeassignment.screens.GameRoomScreen
+import com.example.tictactoeassignment.screens.LoginScreen
+import com.example.tictactoeassignment.viewModels.GameRoomViewModel
+import com.example.tictactoeassignment.viewModels.LoginViewModel
+
+
+@Composable
+fun SetupNavGraph(navController : NavHostController,loginViewModel: LoginViewModel,gameRoomViewModel: GameRoomViewModel){
+        NavHost(navController = navController, startDestination =Screen.LoginScreen.route ){
+            composable(
+                route = Screen.LoginScreen.route
+            ){
+                LoginScreen(viewModel = loginViewModel,navController = navController)
+            }
+
+            composable(
+                route = Screen.GameRoomScreen.route
+            ){
+                GameRoomScreen(viewModel = gameRoomViewModel)
+            }
+        }
+}
---
+++ app/src/main/java/com/example/tictactoeassignment/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package com.example.tictactoeassignment.navigation
+
+sealed class Screen(val route : String = "") {
+    object LoginScreen : Screen("login_screen")
+    object GameRoomScreen : Screen("game_room_screen")
+    object GameScreen : Screen("game_screen")
+    object SZero : Screen()
+}
