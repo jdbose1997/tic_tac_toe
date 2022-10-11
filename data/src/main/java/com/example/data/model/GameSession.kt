ec5676b4b52b9e47ebc3b68a8f7468906f1595b9
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 14:37:43 +0530

    add new member vars

--- data/src/main/java/com/example/data/model/GameSession.kt
+++ data/src/main/java/com/example/data/model/GameSession.kt
@@ -3,10 +3,14 @@ package com.example.data.model
 data class GameSession(
     val playerMoves : MutableMap<String,String>,
     val currentTurn : String,
-    val lastPlayerMoveId : String,
+    val currentPlayerId : String,
+    val firstPlayerId : String,
+    val secondPlayerId : String,
+    var hasWon : Boolean = false,
     var timer : Int = 30
+
 ){
     constructor() : this(
-        mutableMapOf(),"CROSS",""
+        mutableMapOf(),"CROSS","","",""
     )
 }
