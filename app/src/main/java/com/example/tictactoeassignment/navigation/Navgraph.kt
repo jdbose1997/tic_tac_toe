9f40c215165f5fb5f7b84e6450dca028d5a5068e
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 14:44:59 +0530

    chore: Add other screens to navigate

--- app/src/main/java/com/example/tictactoeassignment/navigation/Navgraph.kt
+++ app/src/main/java/com/example/tictactoeassignment/navigation/Navgraph.kt
@@ -4,16 +4,22 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.tictactoeassignment.screens.GameLobbyScreen
 import com.example.tictactoeassignment.screens.GameRoomScreen
 import com.example.tictactoeassignment.screens.GameScreen
 import com.example.tictactoeassignment.screens.LoginScreen
+import com.example.tictactoeassignment.viewModels.GameLobbyViewModel
 import com.example.tictactoeassignment.viewModels.GameRoomViewModel
 import com.example.tictactoeassignment.viewModels.GameViewModel
 import com.example.tictactoeassignment.viewModels.LoginViewModel
 
 
 @Composable
-fun SetupNavGraph(navController : NavHostController,loginViewModel: LoginViewModel,gameRoomViewModel: GameRoomViewModel,gameViewModel: GameViewModel){
+fun SetupNavGraph(navController : NavHostController,
+                  loginViewModel: LoginViewModel,
+                  gameRoomViewModel: GameRoomViewModel,
+                  gameViewModel: GameViewModel,
+                  gameLobbyViewModel: GameLobbyViewModel){
         NavHost(navController = navController, startDestination =Screen.LoginScreen.route ){
             composable(
                 route = Screen.LoginScreen.route
@@ -31,6 +37,12 @@ fun SetupNavGraph(navController : NavHostController,loginViewModel: LoginViewMod
                 route = Screen.GameScreen.route
             ){
                 GameScreen(viewModel = gameViewModel)
+            }
+
+            composable(
+                route = Screen.GameLobbyScreen.route
+            ){
+                GameLobbyScreen(viewModel = gameLobbyViewModel)
             }
         }
 }
