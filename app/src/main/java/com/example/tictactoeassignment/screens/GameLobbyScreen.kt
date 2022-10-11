5ef92849b694379f11692e51c56f798fa7d09619
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 16:05:43 +0530

    feat: Add a button to start the game

--- app/src/main/java/com/example/tictactoeassignment/screens/GameLobbyScreen.kt
+++ app/src/main/java/com/example/tictactoeassignment/screens/GameLobbyScreen.kt
@@ -5,15 +5,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.tictactoeassignment.navigation.Screen
 import com.example.tictactoeassignment.viewModels.GameLobbyViewModel
 
 @Composable
-fun GameLobbyScreen( viewModel : GameLobbyViewModel){
+fun GameLobbyScreen(navHostController: NavHostController,viewModel : GameLobbyViewModel){
 
     val timerUi = viewModel.gameStartTimer.collectAsState().value
 
@@ -21,5 +24,10 @@ fun GameLobbyScreen( viewModel : GameLobbyViewModel){
        Text(text = "Please wait")
        Spacer(modifier = Modifier.height(15.dp))
         Text(text = "The Game will start in ... ${timerUi}")
+        Button(onClick = {
+            viewModel.getPlayerDetailsFromGameRoomAndCreateGameSession()
+            navHostController.navigate(Screen.GameScreen.route)}) {
+            Text("Start")
+        }
     }
 }
