73a3da2f2368963fdfa9b50524fd775936d00a6e
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 11:24:11 +0530

    add new parameter for currentPlayerId in gamesession

--- data/src/main/java/com/example/data/model/GameSession.kt
+++ data/src/main/java/com/example/data/model/GameSession.kt
@@ -2,9 +2,10 @@ package com.example.data.model
 
 data class GameSession(
     val playerMoves : MutableMap<String,String>,
-    val currentTurn : String
+    val currentTurn : String,
+    val currentPlayerId : String
 ){
     constructor() : this(
-        mutableMapOf(),"CROSS"
+        mutableMapOf(),"CROSS",""
     )
 }
