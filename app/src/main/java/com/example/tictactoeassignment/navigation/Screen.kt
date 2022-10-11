90e49a8008ba653654e3788564fb16e785a1d4dd
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 14:43:52 +0530

    chore: Add Gamelobby screen in navigation screen

--- app/src/main/java/com/example/tictactoeassignment/navigation/Screen.kt
+++ app/src/main/java/com/example/tictactoeassignment/navigation/Screen.kt
@@ -3,6 +3,7 @@ package com.example.tictactoeassignment.navigation
 sealed class Screen(val route : String = "") {
     object LoginScreen : Screen("login_screen")
     object GameRoomScreen : Screen("game_room_screen")
+    object GameLobbyScreen : Screen("game_lobby_screen")
     object GameScreen : Screen("game_screen")
     object SZero : Screen()
 }
