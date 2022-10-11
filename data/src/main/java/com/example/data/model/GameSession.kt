83eb7959345cbf61e062811297bcc3d0e70f0e13
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 11:25:30 +0530

    add lastPlayerMoveId and timer in gamesession

--- data/src/main/java/com/example/data/model/GameSession.kt
+++ data/src/main/java/com/example/data/model/GameSession.kt
@@ -3,7 +3,8 @@ package com.example.data.model
 data class GameSession(
     val playerMoves : MutableMap<String,String>,
     val currentTurn : String,
-    val currentPlayerId : String
+    val lastPlayerMoveId : String,
+    var timer : Int = 30
 ){
     constructor() : this(
         mutableMapOf(),"CROSS",""
