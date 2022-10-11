7abb9187116b1c792bb2a9c06b9ca8c15f816dac
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-11 16:07:14 +0530

    chore: On successfull login navigate to GameRoomScreen

--- app/src/main/java/com/example/tictactoeassignment/screens/LoginScreen.kt
+++ app/src/main/java/com/example/tictactoeassignment/screens/LoginScreen.kt
@@ -55,7 +55,7 @@ fun LoginScreen(viewModel: LoginViewModel, navController: NavHostController){
                     userName = userNameField
                 )
             )
-            if(navigateToUi == Screen.GameLobbyScreen.route){
+            if(navigateToUi == Screen.GameRoomScreen.route){
                 navController.navigate(navigateToUi)
             }
         }) {
--- app/src/main/java/com/example/tictactoeassignment/viewModels/LoginViewModel.kt
+++ app/src/main/java/com/example/tictactoeassignment/viewModels/LoginViewModel.kt
@@ -55,7 +55,7 @@ class LoginViewModel @Inject constructor(
                     if(player != null){
                         //To Game Screen
                         savePlayerData(player)
-                        navigateToUi = Screen.GameLobbyScreen.route
+                        navigateToUi = Screen.GameRoomScreen.route
                     }else{
                         //Create New Player
                         createNewUser(
