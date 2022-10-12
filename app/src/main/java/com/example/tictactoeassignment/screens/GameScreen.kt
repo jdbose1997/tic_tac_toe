b5969d5c5a55c0b92d0f6b9770eeaa8520268f09
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-12 12:43:38 +0530

    refactor: Add navhostcontroller

--- app/src/main/java/com/example/tictactoeassignment/screens/GameScreen.kt
+++ app/src/main/java/com/example/tictactoeassignment/screens/GameScreen.kt
@@ -1,11 +1,5 @@
 package com.example.tictactoeassignment.screens
 
-import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -18,7 +12,6 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,9 +20,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.domain.BoardCellValue
 import com.example.domain.GameState
 import com.example.domain.PlayerAction
@@ -40,6 +33,7 @@ import com.example.tictactoeassignment.viewModels.GameViewModel
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun GameScreen(
+    navHostController: NavHostController,
     viewModel: GameViewModel,
     sessionId : String
 ) {
