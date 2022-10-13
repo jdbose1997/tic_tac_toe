235c280a7887f0bc89e290a40ecf4eac23cca833
Joydip Bose <joydipbose@Joydips-MacBook-Pro.local>
2022-10-13 10:37:01 +0530

    refactor : Use proper usecases

--- app/src/main/java/com/example/tictactoeassignment/viewModels/GameRoomViewModel.kt
+++ app/src/main/java/com/example/tictactoeassignment/viewModels/GameRoomViewModel.kt
@@ -2,38 +2,31 @@ package com.example.tictactoeassignment.viewModels
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.data.model.GameRoom
 import com.example.data.model.Player
-import com.example.data.model.repository.GameRoomRepository
-import com.example.data.model.repository.GameRoomRepositoryImpl
-import com.example.data.model.repository.PlayerRepository
+import com.example.domain.useCases.FetchGameRoomsUseCase
+import com.example.domain.useCases.GetCurrentPlayerDataUseCase
+import com.example.domain.useCases.JoinGameRoomUseCase
 import com.example.tictactoeassignment.Constant.TAG
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import java.util.Random
-import java.util.UUID
+import java.util.*
 import javax.inject.Inject
 
 @SuppressLint("MutableCollectionMutableState")
 @HiltViewModel
 class GameRoomViewModel @Inject constructor(
-    private val playerRepository: PlayerRepository
+    private val getCurrentPlayerDataUseCase: GetCurrentPlayerDataUseCase,
+    private val fetchGameRoomsUseCase: FetchGameRoomsUseCase,
+    private val joinGameRoomUseCase: JoinGameRoomUseCase
 ): ViewModel() {
-    private val gameRepository : GameRoomRepository = GameRoomRepositoryImpl(Firebase.firestore)
+
     var roomList : MutableStateFlow<List<GameRoom>> = MutableStateFlow(emptyList())
     private var player : Player ?= null
 
@@ -42,7 +35,7 @@ class GameRoomViewModel @Inject constructor(
     }
 
     private fun fetchPlayerObject(){
-        playerRepository.getPlayerData().onEach {
+        getCurrentPlayerDataUseCase().onEach {
             if(it != null){
                 player = it
                 fetchAllGameRooms(it._id)
@@ -50,34 +43,8 @@ class GameRoomViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
-    private fun getUserAsModel(): Flow<List<Player>> {
-        val playerList = ArrayList<Player>()
-        return callbackFlow {
-            val db = Firebase.firestore
-            val listRef = db.collection("players")
-
-            listRef.addSnapshotListener { value, error ->
-                playerList.clear()
-                if(error != null){
-                    cancel(error.message.toString())
-                }
-                if(value != null &&  value.documents.isNotEmpty()){
-                    value.documents.forEach {
-                        val player = it.toObject(Player::class.java)
-                        player?.let {
-                            playerList.add(player)
-                        }
-                    }
-                    trySend(playerList)
-                }
-            }
-            awaitClose { close() }
-        }
-
-    }
-
     private fun fetchAllGameRooms(playerId : String){
-        gameRepository.fetchGameRooms().onEach {gameRoomList->
+        fetchGameRoomsUseCase().onEach {gameRoomList->
             viewModelScope.launch {
                 val job = async {
                     gameRoomList.forEach {
@@ -95,20 +62,7 @@ class GameRoomViewModel @Inject constructor(
      fun addPlayerToGameRoom(
         roomId : String
     ){
-        gameRepository.checkIfUserCanJoinRoom(roomId = roomId).onEach {
-            if(it){
-                gameRepository.fetchGameRoomUsingId(roomId).onEach {
-                    it.apply {
-                        if(player != null && !playerAllReadyJoined(players,player?._id.toString())){
-                            currentPlayers += 1
-                            players.add(player!!)
-                            gameRepository.joinGameRoom(gameRoom = this,roomId).launchIn(viewModelScope)
-                        }
-                    }
-                }.launchIn(viewModelScope)
-            }
-
-        }.launchIn(viewModelScope)
+         player?.let { joinGameRoomUseCase.invoke(player = it, roomId = roomId,viewModelScope) }
     }
 
 
